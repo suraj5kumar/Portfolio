e29ca3e58001d5d4c241c3088c671cbae68fcd13
Suraj Kumar <suraj6549kumar@gmail.com>
2024-03-29 18:12:10 +0530

    Project request form for portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        "350": "350px",
+        "650": "650px",
+        "850": "850px",
+        "1100": "1100px",
+        "1300": "1300px"
+      },
+      height: {
+        "350": "350px",
+        "650": "650px",
+        "850": "850px",
+        "1100": "1100px",
+        "1300": "1300px"
+      }
+    },
+  },
+  plugins: [],
+}
+
